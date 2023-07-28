f934498f42b2af077a68eadc04d5557f33203d3c
JulianmLacey <Julianmlacey@gmail.com>
2023-07-28 02:38:42 -0700

    added SVG string generation functionality

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-//prompt inquirer
-    //text - up too 3 char
-    //text color
-    //list of shapes (circle, color, square)
-    //shape color
+var inquirer = require("inquirer");
 
+const shapes = require("./lib/shapes");
 
-    var inquirer = require("inquirer");
-
-    const shapes = require('./lib/shapes');
-
-    function getUserData(){
-        //Prompt User
-        inquirer.prompt([
-            {type: "input",message: "Text: ",name: "text", },
-            {type: "input",message: "Text Color: ",name: "Tcolor", },
-            {type: "input",message: "Shape Color: ",name: "Scolor", },
-            {type: "list", message: "Shape: ", name: "shape", 
-            choices: ["Circle", "Triangle", "Square"] }])
-            .then(answers => { shapes.generateSVG(answers);});//pass answers to file generation
-
-            
-    }
-    getUserData();
+function getUserData() {
+	//Prompt User
+	inquirer
+		.prompt([
+			{ type: "input", message: "Text: ", name: "text" },
+			{ type: "input", message: "Text Color: ", name: "Tcolor" },
+			{ type: "input", message: "Shape Color: ", name: "Scolor" },
+			{
+				type: "list",
+				message: "Shape: ",
+				name: "shape",
+				choices: ["Circle", "Triangle", "Square"],
+			},
+		])
+		.then((answers) => {
+			if (answers.text.length > 3) {
+				console.log("Error: Text Greater Than 3 Characters");
+				return;
+			}
+			shapes.generateSVG(answers);
+		}); //pass answers to file generation
+}
+getUserData();
